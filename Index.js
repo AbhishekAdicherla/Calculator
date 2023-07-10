a7759280aebd24e517f6144646d52302ee9396f2
AbhishekAdicherla <138871801+AbhishekAdicherla@users.noreply.github.com>
2023-07-10 19:55:51 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,60 @@
+var clickSound1=new Audio()
+var clickSound2=new Audio()
+var clickSound3=new Audio()
+var clickSound4=new Audio()
+var clickSound5=new Audio()
+var clickSound6=new Audio()
+var clickSound7=new Audio()
+var clickSound8=new Audio()
+var clickSound9=new Audio()
+var clickSound10=new Audio()
+clickSound1.src="./click1.mp3"
+clickSound2.src="./click2.mp3"
+clickSound3.src="./click3.mp3"
+clickSound4.src="./click4.mp3"
+clickSound5.src="./click5.mp3"
+clickSound6.src="./click6.mp3"
+clickSound7.src="./click7.mp3"
+clickSound8.src="./click8.mp3"
+clickSound9.src="./click9.mp3"
+clickSound10.src="./click10.mp3"
+function clickAudio1()
+{
+    clickSound1.play()
+}
+function clickAudio2()
+{
+    clickSound2.play()
+}
+function clickAudio3()
+{
+    clickSound3.play()
+}
+function clickAudio4()
+{
+    clickSound4.play()
+}
+function clickAudio5()
+{
+    clickSound5.play()
+}
+function clickAudio6()
+{
+    clickSound6.play()
+}
+function clickAudio7()
+{
+    clickSound7.play()
+}
+function clickAudio8()
+{
+    clickSound8.play()
+}
+function clickAudio9()
+{
+    clickSound9.play()
+}
+function clickAudio10()
+{
+    clickSound10.play()
+}
